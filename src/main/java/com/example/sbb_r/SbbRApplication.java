7585d4893fe4bdcf3b55cb4a3ea05c7c090b1888
grirorevvadim <grigoryevvadim@mail.ru>
2022-02-19 21:40:15 +0300

    security and password encryption are added

--- src/main/java/com/example/sbb_r/SbbRApplication.java
+++ src/main/java/com/example/sbb_r/SbbRApplication.java
@@ -2,12 +2,19 @@ package com.example.sbb_r;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SbbRApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SbbRApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/example/sbb_r/servises/implementations/UserServiceImpl.java
+++ src/main/java/com/example/sbb_r/servises/implementations/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.sbb_r.models.entities.User;
 import com.example.sbb_r.repositories.UserRepository;
 import com.example.sbb_r.servises.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -12,11 +13,13 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     @Transactional
     public User createUser(User user) {
         user.setEmailVerificationStatus(true);
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return user;
     }
